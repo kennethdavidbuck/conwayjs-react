10e5c01f03dfd84364e80592399aaadb3d7b8c01
Kenneth Buck <kenneth@nestedsphere.com>
2016-08-07 14:32:25 -0400

    Shrink table size to 25x25. Add h1

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -7,8 +7,8 @@ class App extends React.Component {
   constructor(props) {
     super(...arguments);
     this.game = new Conway(Conway.createBoard({
-      width: 50,
-      height: 50
+      width: 25,
+      height: 25
     }));
     this.state = { board: this.game.board };
     this.wait = 250;
@@ -16,7 +16,11 @@ class App extends React.Component {
 
   render() {
     const rows = this.state.board.map((row, index) => (
-      <Row key={index} rowIndex={index} cells={row} toggleCell={this.handleToggleCell.bind(this)}/>
+      <Row
+        key={index}
+        rowIndex={index}
+        cells={row}
+        toggleCell={this.handleToggleCell.bind(this)}/>
     ));
 
     return (
